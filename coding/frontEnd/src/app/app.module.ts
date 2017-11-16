ade45acff0314770307bbcaac815418ca281ec58
Prashant Dhirendra <prashant.dhiru@gmail.com>
2017-11-16 11:41:11 +0530

    matarializedCSS CDN and Register Component made

--- coding/frontEnd/src/app/app.module.ts
+++ coding/frontEnd/src/app/app.module.ts
@@ -5,10 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './user/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,

7f67dfa084f7ce6f455e7caa6cf708d8f1b648a8
Prashant Dhirendra <prashant.dhiru@gmail.com>
2017-11-16 19:00:16 +0530

    Login Form and Registration Form created
    navBar compoment made, now have to work on it

--- coding/frontEnd/src/app/app.module.ts
+++ coding/frontEnd/src/app/app.module.ts
@@ -3,15 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './user/register/register.component';
+//services
+import { RegisterService } from './services/register.service';
+import { LoginComponent } from './user/login/login.component';
+import { NavBarComponent } from './common/nav-bar/nav-bar.component'
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
+    LoginComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,7 +27,7 @@ import { RegisterComponent } from './user/register/register.component';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [RegisterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
